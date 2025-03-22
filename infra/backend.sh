89dbce67cba3a2153e725626c45b01f7a4a3949f
Van Labajo <73100124+vanlabajo@users.noreply.github.com>
2025-03-22 15:42:13 -0700

    feat: added backend.sh that preps remote backend of tfstate

---
+++ infra/backend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./config.sh
+
+RESOURCE_GROUP_NAME=rg-tfstate
+STORAGE_ACCOUNT_NAME=aichatbot$RANDOM
+CONTAINER_NAME=tfstate
+
+# Login
+az login
+
+# Switch to Visual Studio Subscription
+az account set --subscription $SUBSCRIPTION_ID
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location centralus
+
+# Register the Resource Provider
+az provider register --namespace 'Microsoft.Storage'
+
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME

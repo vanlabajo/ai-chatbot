d4bb16decbe9126a264fff88325c0ae5aba63466
Van Labajo <73100124+vanlabajo@users.noreply.github.com>
2025-06-04 13:15:38 -0700

    feat: use azure key vault

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,6 +11,8 @@ resource "random_string" "random" {
   numeric = true
 }
 
+data "azurerm_client_config" "current" {}
+
 # Resource Group
 resource "azurerm_resource_group" "ai_rg" {
   name     = "rg-ai-chatbot-${random_string.random.result}"
@@ -64,17 +66,17 @@ resource "azurerm_linux_web_app" "chatbot_api" {
   }
 
   app_settings = {
-    "AzureOpenAI__ApiKey"                   = azurerm_cognitive_account.openai.primary_access_key
-    "AzureOpenAI__Endpoint"                 = azurerm_cognitive_account.openai.endpoint
-    "AzureOpenAI__DeploymentName"           = azurerm_cognitive_deployment.openai.name
-    "AllowedOrigins__0"                     = "https://${azurerm_linux_web_app.chatbot_ui.default_hostname}"
+    "AzureOpenAI__ApiKey"                   = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.openai_api_key.id})"
+    "AzureOpenAI__Endpoint"                 = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.openai_endpoint.id})"
+    "AzureOpenAI__DeploymentName"           = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.openai_deployment_name.id})"
+    "AllowedOrigins__0"                     = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.allowed_origins_0.id})"
     "AllowedOrigins__1"                     = ""
     "AllowedOrigins__2"                     = ""
     "AllowedOrigins__3"                     = ""
-    "ApplicationInsights__ConnectionString" = azurerm_application_insights.chatbot_insights.connection_string
-    "CosmosDb__Endpoint"                    = azurerm_cosmosdb_account.chatbot_cosmos.endpoint
-    "CosmosDb__DatabaseName"                = azurerm_cosmosdb_sql_database.chatbot_db.name
-    "CosmosDb__ContainerName"               = azurerm_cosmosdb_sql_container.chatbot_container.name
+    "ApplicationInsights__ConnectionString" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.appinsights_connection_string.id})"
+    "CosmosDb__Endpoint"                    = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.cosmosdb_endpoint.id})"
+    "CosmosDb__DatabaseName"                = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.cosmosdb_database_name.id})"
+    "CosmosDb__ContainerName"               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.cosmosdb_container_name.id})"
   }
 }
 
@@ -201,4 +203,71 @@ resource "azurerm_role_assignment" "cosmos_db_contributor" {
   scope                = azurerm_cosmosdb_account.chatbot_cosmos.id
   role_definition_name = "Cosmos DB Built-in Data Contributor"
   principal_id         = azurerm_user_assigned_identity.chatbot_api_identity.principal_id
+}
+
+# Key Vault
+resource "azurerm_key_vault" "chatbot_key_vault" {
+  name                = "chatbot-keyvault-${random_string.random.result}"
+  location            = azurerm_resource_group.ai_rg.location
+  resource_group_name = azurerm_resource_group.ai_rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.chatbot_api_identity.principal_id
+
+    key_permissions         = ["Get", "List"]
+    secret_permissions      = ["Get", "List"]
+    certificate_permissions = ["Get", "List"]
+  }
+}
+
+# Store secrets in Key Vault
+resource "azurerm_key_vault_secret" "openai_api_key" {
+  name         = "AzureOpenAI--ApiKey"
+  value        = azurerm_cognitive_account.openai.primary_access_key
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "openai_endpoint" {
+  name         = "AzureOpenAI--Endpoint"
+  value        = azurerm_cognitive_account.openai.endpoint
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "openai_deployment_name" {
+  name         = "AzureOpenAI--DeploymentName"
+  value        = azurerm_cognitive_deployment.openai.name
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "allowed_origins_0" {
+  name         = "AllowedOrigins--0"
+  value        = "https://${azurerm_linux_web_app.chatbot_ui.default_hostname}"
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "appinsights_connection_string" {
+  name         = "ApplicationInsights--ConnectionString"
+  value        = azurerm_application_insights.chatbot_insights.connection_string
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "cosmosdb_endpoint" {
+  name         = "CosmosDb--Endpoint"
+  value        = azurerm_cosmosdb_account.chatbot_cosmos.endpoint
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "cosmosdb_database_name" {
+  name         = "CosmosDb--DatabaseName"
+  value        = azurerm_cosmosdb_sql_database.chatbot_db.name
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "cosmosdb_container_name" {
+  name         = "CosmosDb--ContainerName"
+  value        = azurerm_cosmosdb_sql_container.chatbot_container.name
+  key_vault_id = azurerm_key_vault.chatbot_key_vault.id
 }

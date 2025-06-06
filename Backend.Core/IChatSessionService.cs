abf55852238af9406a0fa41b738254204a3afa43
Van Labajo <73100124+vanlabajo@users.noreply.github.com>
2025-06-06 10:55:53 -0700

    feat: added ChatSessionService

---
+++ Backend.Core/IChatSessionService.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Core.Models;
+
+namespace Backend.Core
+{
+    public interface IChatSessionService
+    {
+        Task SaveSessionAsync(ChatSession session);
+        Task<ChatSession?> GetSessionByIdAsync(string userId, string sessionId);
+        Task<IEnumerable<ChatSession>> GetAllSessionsForUserAsync(string userId);
+        Task DeleteSessionAsync(string userId, string sessionId);
+    }
+}
---
+++ Backend.Infrastructure/ChatSessionService.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Core;
+using Backend.Core.Models;
+using Backend.Core.Repositories;
+
+namespace Backend.Infrastructure
+{
+    public class ChatSessionService(IChatSessionRepository chatSessionRepository) : IChatSessionService
+    {
+        private readonly IChatSessionRepository _chatSessionRepository = chatSessionRepository;
+
+        public Task SaveSessionAsync(ChatSession session)
+        {
+            return _chatSessionRepository.SaveSessionAsync(session);
+        }
+
+        public Task<ChatSession?> GetSessionByIdAsync(string userId, string sessionId)
+        {
+            return _chatSessionRepository.GetSessionAsync(userId, sessionId);
+        }
+
+        public Task<IEnumerable<ChatSession>> GetAllSessionsForUserAsync(string userId)
+        {
+            return _chatSessionRepository.GetAllSessionsForUserAsync(userId);
+        }
+
+        public Task DeleteSessionAsync(string userId, string sessionId)
+        {
+            return _chatSessionRepository.DeleteSessionAsync(userId, sessionId);
+        }
+    }
+}

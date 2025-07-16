17a03592e4f590eb5d57987b77fd3bd8dbbb8189
Van Labajo <73100124+vanlabajo@users.noreply.github.com>
2025-07-16 09:28:42 -0700

    refactor(ui): better login page

--- Frontend.Web/src/components/ui/auth/UnauthorizedMessage.tsx
+++ Frontend.Web/src/components/ui/auth/UnauthorizedMessage.tsx
@@ -1,17 +1,18 @@
-import { SignInButton } from "./SignInButton";
+
+import { Logo } from "@/public/Logo"
+import { SignInButton } from "./SignInButton"
 
 export function UnauthorizedMessage() {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">401</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Unauthorized</h1>
-        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">You are not authorized to view this page.</p>
-        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Please login to continue.</p>
-      </div>
-      <div className="mt-10 flex items-center justify-center gap-x-6">
-        <SignInButton />
-      </div>
-    </main>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Logo className="mt-6 h-auto w-32" />
+      <h1 className="mt-4 text-2xl font-semibold text-gray-900 dark:text-gray-50">
+        Welcome to AI Chatbot!
+      </h1>
+      <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+        Please sign in to start a new conversation or continue where you left off.
+      </p>
+      <SignInButton className="mt-8" text="Sign in" />
+    </div>
   )
 }

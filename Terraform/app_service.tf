bfba67b821270b4d2b7bc1c54dc6f4497b5dcd64
Van Labajo <73100124+vanlabajo@users.noreply.github.com>
2025-07-17 11:14:04 -0700

    feat(api): upgrade to .net9

--- Terraform/app_service.tf
+++ Terraform/app_service.tf
@@ -15,7 +15,7 @@ resource "azurerm_linux_web_app" "chatbot_api" {
   site_config {
     always_on = true
     application_stack {
-      dotnet_version = "8.0"
+      dotnet_version = "9.0"
     }
   }
 
